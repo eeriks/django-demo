c6747112539315469d13bda9af46693e68cc0461
Eriks Karls <eriks.karls@28stone.com>
2022-02-11 17:06:25 +0200

    Model names
    Migration

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-class SomeBaseModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+class ParentModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True, null=False)
     modified = models.DateTimeField(auto_now=True)
 
 
-class OtherModel(SomeBaseModel):
+class ChildModel(ParentModel):
     some_field = models.CharField(max_length=12)
