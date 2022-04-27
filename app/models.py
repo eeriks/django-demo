6470b4dcb4848a96b3942894446f9929d76a454f
Eriks Karls <eriks.karls@28stone.com>
2022-04-27 16:36:39 +0300

    OneToOneRelation save/assign order bug
    Migration

--- app/models.py
+++ app/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class ParentModel(models.Model):
     created = models.DateTimeField(auto_now_add=True, null=False)
     modified = models.DateTimeField(auto_now=True)
+    one_to_one_relation = models.OneToOneField("RelatedModel", on_delete=models.CASCADE)
 
 
 class ChildModel(ParentModel):
+    some_field = models.CharField(max_length=12)
+
+
+class RelatedModel(models.Model):
     some_field = models.CharField(max_length=12)
